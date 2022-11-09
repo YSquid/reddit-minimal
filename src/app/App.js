6d8cbabfd4f756f870621a3867745abe53c2f734
Ysquid <ahk648@gmail.com>
2022-11-08 20:33:32 -0600

    API - fetching posts from /pics working

--- src/app/App.js
+++ src/app/App.js
@@ -1,9 +1,15 @@
 import "./App.css";
+import React, {useEffect, useState} from 'react'
 import Header from "../components/header/Header";
 import Posts from "../components/posts/Posts";
 import Subreddits from "../components/subreddits/Subreddits";
+import getPosts from "../api/fetchPosts";
 
 function App() {
+
+  useEffect(() => {
+    getPosts()
+  }, [])
   
   return (
     <div className="App">
