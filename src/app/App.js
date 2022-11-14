57c6a47746957927865419e376eab34cb2213f39
Ysquid <ahk648@gmail.com>
2022-11-14 15:44:27 -0600

    happy with layout down to iPhone SE sizing and up t 2300px width

--- src/app/App.js
+++ src/app/App.js
@@ -1,16 +1,19 @@
 import "./App.css";
-import React from 'react'
+import React from "react";
 import Header from "../components/header/Header";
 import Posts from "../components/posts/Posts";
 import Subreddits from "../components/subreddits/Subreddits";
 
 function App() {
-  
   return (
     <div className="App">
+      <div className="Header">
         <Header />
+      </div>
+      <div className="Body">
         <Posts />
         <Subreddits />
+      </div>
     </div>
   );
 }
