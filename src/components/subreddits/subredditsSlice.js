9230b36cf64f6f90b5024a3aa3f177d1af72ae56
Ysquid <ahk648@gmail.com>
2022-11-14 10:56:57 -0600

    added 10 subreddits

--- src/components/subreddits/subredditsSlice.js
+++ src/components/subreddits/subredditsSlice.js
@@ -1,48 +1,44 @@
 import { createSlice } from "@reduxjs/toolkit";
-import JSicon from '../../assets/JSthumbnail.png'
-import ARicon from '../../assets/question-icon.png'
-import PHicon from '../../assets/PH-icon.jpg'
-
+import JSicon from "../../assets/JSthumbnail.png";
+import ARicon from "../../assets/question-icon.png";
+import PHicon from "../../assets/PH-icon.jpg";
 
 const initialState = {
-    subreddits: [
-        {
-            subredditTitle: 'javascript',
-            icon: JSicon
-        },
-        {
-            subredditTitle: 'AskReddit',
-            icon: ARicon
-        },
-        {
-            subredditTitle: 'ProgrammerHumor',
-            icon: PHicon
-        }
-    ],
-    isLoading: false,
-    errorLoading: false
-}
+  subreddits: [
+    { subredditTitle: "javascript" },
+    { subredditTitle: "ProgrammerHumor" },
+    { subredditTitle: "reactjs" },
+    { subredditTitle: "AskReddit" },
+    { subredditTitle: "gaming" },
+    { subredditTitle: "aww" },
+    { subredditTitle: "Music" },
+    { subredditTitle: "science" },
+    { subredditTitle: "worldnews" },
+    { subredditTitle: "movies" },
+  ],
+  isLoading: false,
+  errorLoading: false,
+};
 
 export const subredditsSlice = createSlice({
-    name: 'subreddits',
-    initialState: initialState,
-    reducers: {
-        addSubreddit: {
-            reducer: (state, action) => {
-                const {subreddit, icon} = action.payload
-                state.subreddits.push({
-                    subreddit: subreddit,
-                    icon: icon
-                })
-            }
-        }
-    }
-})
+  name: "subreddits",
+  initialState: initialState,
+  reducers: {
+    addSubreddit: {
+      reducer: (state, action) => {
+        const { subreddit } = action.payload;
+        state.subreddits.push({
+          subreddit: subreddit,
+        });
+      },
+    },
+  },
+});
 
-export const {addPost} = subredditsSlice.actions //addSubreddit action
+export const { addPost } = subredditsSlice.actions; //addSubreddit action
 
-export const selectSubreddits = (state) => state.subreddits.subreddits //select subreddits piece of subreddits state
-export const selectIsLoading = (state) => state.subreddits.isLoading //select isLoading piece of subreddits state
-export const selectErrorLoading = (state) => state.subreddits.errorLoading //select errorLoading piece of subreddits state
+export const selectSubreddits = (state) => state.subreddits.subreddits; //select subreddits piece of subreddits state
+export const selectIsLoading = (state) => state.subreddits.isLoading; //select isLoading piece of subreddits state
+export const selectErrorLoading = (state) => state.subreddits.errorLoading; //select errorLoading piece of subreddits state
 
-export default subredditsSlice.reducer //default export in the reducer for the slice
+export default subredditsSlice.reducer; //default export in the reducer for the slice
