15680e799b1d8e70239669de0d90b87cbf0fd2aa
Ysquid <ahk648@gmail.com>
2022-11-29 15:22:40 -0600

    comments day/night mode done

--- src/components/comment/Comment.js
+++ src/components/comment/Comment.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import './Comment.css'
+import {useSelector} from 'react-redux'
+import {selectTheme} from '../posts/redditSlice'
 
 function Comment({author, body, created_utc, postedAgo}) {
+  const theme = useSelector(selectTheme)
   return (
-    <div className='Comment'>
+    <div className={`Comment-${theme}`}>
         <h3 className='commentAuthor'>{author}</h3>
-        <p className='commentBody'>{body}</p>
-        <p className='commentPostedAgo'>{postedAgo(created_utc)}</p>
+        <p className={`commentBody-${theme}`}>{body}</p>
+        <p className={`commentPostedAgo-${theme}`}>{postedAgo(created_utc)}</p>
     </div>
   )
 }
